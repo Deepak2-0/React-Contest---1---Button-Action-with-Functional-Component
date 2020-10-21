24fd432d201e5f9b78db29d2d3e231e52192c22e
Deepak Choudhary <choudharydeepak938@gmail.com>
2020-10-21 21:35:25 +0530

    put boolean in state

--- src/components/App.js
+++ src/components/App.js
@@ -2,21 +2,22 @@ import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
 function App() {
-  const [text, setText] = useState({ id: "", para: "" });
+  const [text, setText] = useState(false);
 
   function handleClick() {
-    setText({
-      id: "para",
-      para:
-        "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"
-    });
+    setText(true);
   }
   return (
     <div id="main">
       <button id="click" onClick={handleClick}>
         Click
       </button>
-      <p id={text.id}>{text.para}</p>
+      {text && (
+        <p id="para">
+          Hello, I've learnt to use the full-stack evaluation tool. This makes
+          me so happy
+        </p>
+      )}
     </div>
   );
 }
